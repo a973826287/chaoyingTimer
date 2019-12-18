1b2885e58980acdb73af73d23194d15c8c494bb5
XU YUQI <32167659+a973826287@users.noreply.github.com>
2019-12-18 15:40:31 +0800

    demo
    demo

---
+++ 超颖时钟/pages/setting/setting.js
@@ -0,0 +1,46 @@
+const innerAudioContext = wx.createInnerAudioContext()
+innerAudioContext.autoplay = false
+innerAudioContext.src = 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46'
+
+innerAudioContext.onError((res) => {
+  console.log(res.errMsg)
+  console.log(res.errCode)
+})
+Page({
+  data:{
+    current: {
+      poster: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+      name: '此时此刻',
+      author: '许巍',
+      src: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
+    },
+    audioAction: {
+      method: 'pause'
+    },
+
+  },
+  onShow: function() {
+    wx.setNavigationBarTitle({
+      title: '设置'
+    })
+    this.setData({
+    	workTime: wx.getStorageSync('workTime'),
+    	restTime: wx.getStorageSync('restTime')
+    })
+  },
+  changeWorkTime: function(e) {
+  	wx.setStorage({
+  		key: 'workTime',
+  		data: e.detail.value
+  	})
+  },
+  changeRestTime: function(e) {
+  	wx.setStorage({
+  		key: 'restTime',
+  		data: e.detail.value
+  	})
+  },
+  playAudio:function(e){
+    innerAudioContext.play()
+  }
+})
